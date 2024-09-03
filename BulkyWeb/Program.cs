3752c971990a828830c077470c32260b4c010c54
Hazem Ya-sin <hazem.yasin.1416@gmail.com>
2024-09-03 16:17:53 +0300

    will start lecture 32

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,11 +1,22 @@
+//! this part starts building the application
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//this part starts adding service like tools to the application itself
+//the first tool to be added is the controler tool so we can use it later
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//pipeling means that when a request comes to the applcation, how do you want to process that request
+
+//the if statment is to throw an error if the application is runing but no In Development
+//now if we are working in production what shall we do ?
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -13,6 +24,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+//this is related to the wwwroot file
+//this to allow the app to use the wwwroot
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
