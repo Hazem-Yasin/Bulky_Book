e57407ec8b4b11e5baa9ca929a0c7e5242d8f1e9
Hazem Ya-sin <hazem.yasin.1416@gmail.com>
2024-09-05 15:24:13 +0300

    added server side vlaidations

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -11,5 +11,18 @@ namespace BulkyWeb.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Horror", DisplayOrder = 4 },
+                new Category { Id = 5, Name = "Thriller", DisplayOrder = 5 },
+                new Category { Id = 6, Name = "Drama", DisplayOrder = 6 },
+                new Category { Id = 7, Name = "Romance", DisplayOrder = 7 }
+                );
+        }
     }
 }
