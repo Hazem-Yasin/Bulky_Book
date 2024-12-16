3243e786a9c21a0778ed1fb2d55ddc795d143ac2
Hazem Ya-sin <hazem.yasin.1416@gmail.com>
2024-12-16 02:26:48 +0200

    testing git push

--- SandBox/Bulky/BulkyWeb/Program.cs
+++ SandBox/Bulky/BulkyWeb/Program.cs
@@ -19,7 +19,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+//testing more git 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
